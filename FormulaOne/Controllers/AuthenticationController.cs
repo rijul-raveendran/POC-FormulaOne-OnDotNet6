1f547e1325717b2062e9f1f431ddf9fb023600b7
Rijul Raveendran <rijulcit@gmail.com>
2023-04-15 21:44:54 +0530

    Verify/Confirm Email

--- FormulaOne/Controllers/AuthenticationController.cs
+++ FormulaOne/Controllers/AuthenticationController.cs
@@ -3,11 +3,15 @@ using FormulaOne.Data;
 using FormulaOne.Data.DTOs;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration.UserSecrets;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using RestSharp;
+using RestSharp.Authenticators;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using System.Text.Encodings.Web;
 
 namespace FormulaOne.Controllers
 {
@@ -17,11 +21,14 @@ namespace FormulaOne.Controllers
     {
         private readonly JwtConfig _jwtConfig;
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly IConfiguration _configuration;
 
-        public AuthenticationController(UserManager<IdentityUser> userManager, IOptionsMonitor<JwtConfig> optionsMonitor)
+        public AuthenticationController(UserManager<IdentityUser> userManager, IOptionsMonitor<JwtConfig> optionsMonitor, IConfiguration configuration)
         {
             _userManager = userManager;
             _jwtConfig = optionsMonitor.CurrentValue;
+            _configuration = configuration;
+
         }
 
         [HttpPost]
@@ -54,7 +61,8 @@ namespace FormulaOne.Controllers
             var newUser = new IdentityUser()
             {
                 Email = registrationRequestDto.Email,
-                UserName = registrationRequestDto.Name
+                UserName = registrationRequestDto.Name,
+                EmailConfirmed = false
             };
 
             // Add user to the table
@@ -69,14 +77,70 @@ namespace FormulaOne.Controllers
 ,               });
             }
 
+            //var code = await _userManager.GenerateEmailConfirmationTokenAsync(newUser);
+            //var emailBody = "Please confirm your email by clicking on the link below: " +
+            //    $"<a href=\"#URL#\">Confirm Email</a>";
+
+            ////sample format https://localhost:44300/api/authentication/confirmemail?userid={newUser.Id}&code={code}
+            //var callBack_url = Request.Scheme + "://" + Request.Host + Url.Action(nameof(ConfirmEmail), "Authentication", new {userId = newUser.Id, code = code});
+
+            ////var body = emailBody.Replace("#URL#",HtmlEncoder.Default.Encode(callBack_url));
+            //var body = emailBody.Replace("#URL#",callBack_url);
+
+            //// Send email
+            //var result = SendEmail(body, newUser.Email);
+
+            //if (result)
+            //{
+            //    return Ok("Please confirm the email");
+            //}
+
+            //return Ok("Please request an email verification link");
+
+
             // Generate JWT token when user added to table
             var jwtToken = GenerateJwtToken(newUser);
+
+       
 
             return Ok(new AuthResult()
             {
                 IsSuccess = true,
                 Token = jwtToken
             });
+        }
+
+        [Route(nameof(ConfirmEmail))]
+        [HttpGet]
+        public async Task<IActionResult> ConfirmEmail(string userId, string code)
+        {
+            if(userId == null || code == null)
+            {
+                return BadRequest(new AuthResult()
+                {
+                    IsSuccess = false,
+                    Errors = new List<string>() { "Invalid Email confirmation url" }
+                });
+            }
+
+            var user = _userManager.FindByIdAsync(userId);
+
+            if(user == null)
+            {
+                return BadRequest(new AuthResult()
+                {
+                    IsSuccess = false,
+                    Errors = new List<string>() { "Invalid Email parameters" }
+                });
+            }
+
+            //code = Encoding.UTF8.GetString(Convert.FromBase64String(code));
+            var result = await _userManager.ConfirmEmailAsync(user.Result, code);
+            var confirmationStatusMessage = result.Succeeded ? "Thank you for confirming the email"
+                : "Your email is not confirmed, please try again later";
+
+            return Ok(confirmationStatusMessage);
+
         }
 
         [HttpPost]
@@ -104,6 +168,16 @@ namespace FormulaOne.Controllers
                     Errors = new List<string>() { "Invalid Credentials" }
                 });
             }
+
+            //if(!existing_user.EmailConfirmed)
+            //{
+            //    return BadRequest(new AuthResult
+            //    {
+            //        IsSuccess = false,
+
+            //        Errors = new List<string>() { "Please confirm your email" }
+            //    });
+            //}
 
             var isPasswordCorrect = await _userManager.CheckPasswordAsync(existing_user, loginRequestDto.Password);
 
@@ -158,5 +232,31 @@ namespace FormulaOne.Controllers
 
             return jwtToken;
         }
+
+        private bool SendEmail(string emailBody ,string email)
+        {
+            // create client
+            var client = new RestClient("https://api.mailgun.net/v3");
+            // create request
+            var request = new RestRequest("", Method.Post);
+
+            // Replace appsettings.json with original api key
+            client.AddDefaultHeader("api", _configuration["MailGun:ApiKey"]);
+            //client.Authenticator = new HttpBasicAuthenticator("api", _configuration["MailGun:ApiKey"]);
+            request.AddParameter("domain", "sandboxddca8e8c38db41ad93a9204b24827ca0.mailgun.org", ParameterType.UrlSegment);
+            request.Resource = "{domain}/messages";
+            request.AddParameter ("from", "Excited User <mailgun@sandboxddca8e8c38db41ad93a9204b24827ca0.mailgun.org>");
+            //request.AddParameter ("to", email);
+            request.AddParameter("to", "temporaryEmailAddress"); //Mailgain supports only Authorized recepients
+            request.AddParameter ("subject", "Hello");
+            request.AddParameter ("text", emailBody);
+            request.Method = Method.Post;
+            var response = client.Execute(request);
+
+            return response.IsSuccessful;
+
+        }
+
+
     }
 }
